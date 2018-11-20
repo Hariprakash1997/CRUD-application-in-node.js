6b392771ae528a700b6ed28efec408b943f5163b
Hariprakash1997 <1997hariprakash@gmail.com>
2018-11-20 14:30:24 +0530

    added list option and advanced yargs

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-console.log('Starting App...');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOption = {
+    describe: 'Title of the note to read',
+    demand: true,
+    alias: 't'
+};
+
+const bodyOption = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+.command('add', 'Add a new note', {
+    title: titleOption,
+    body: bodyOption
+})
+.command('list', 'List all notes')
+.command('read', 'Read specific notes', {
+    title: titleOption
+})
+.command('delete', 'Delete a note', {
+    title: titleOption
+})
+.help()
+.argv;
+
 var command = argv._[0];
 
 if (command === 'add') {
@@ -20,7 +44,9 @@ if (command === 'add') {
     }
 
 } else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach(note => notes.logNote(note));
 } else if (command === 'read') {
     var note = notes.getNote(argv.title);
     if(note) {
